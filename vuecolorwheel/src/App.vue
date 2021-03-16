c31dec47137536472cc32387d7ebe384993e9213
Brianhin94 <brianhin64@gmail.com>
2021-03-16 12:23:13 -0600

    YES

--- vuecolorwheel/src/App.vue
+++ vuecolorwheel/src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Vue Color Picker Wheel</h1>
+    <p>
+      A Vue color picker component based on the Farbtastic jQuery Color Picker
+      plug-in.
+    </p>
+    <color-picker v-model="color"></color-picker>
+    <div class="selected-color-info">
+      <p>Selected color:</p>
+      <svg height="32" width="32">
+        <circle cx="16" cy="16" r="15" :fill="color" />
+      </svg>
+    </div>
+    <p>{{ color }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ColorPicker from "vue-color-picker-wheel";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    ColorPicker
+  },
+  data() {
+    return {
+      color: "#ff6600"
+    };
   }
-}
+};
 </script>
 
 <style>
+@import url(https://fonts.googleapis.com/css?family=Barlow);
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Barlow", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  text-align: center;
+  margin-top: 20px;
 }
+
+#color-wheel {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.selected-color-info {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+}
+
+
 </style>
+

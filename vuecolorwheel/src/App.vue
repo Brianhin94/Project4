f0c173076d1a563d0eee3c47784fd005fdcae226
Brianhin94 <brianhin64@gmail.com>
2021-03-17 13:31:55 -0600

    Adding social links

--- vuecolorwheel/src/App.vue
+++ vuecolorwheel/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
     <h1>Vue Color Picker Wheel</h1>
-    <p>
-     
-    </p>
     <color-picker v-model="color"></color-picker>
     <div class="selected-color-info">
       <p>Copy this color for your code!:</p>
@@ -12,6 +9,26 @@
       </svg>
     </div>
     <p>{{ color }}</p>
+    <h8
+      ><p>
+        Find more of my work on
+        <a
+          href="https://github.com/Brianhin94"
+          title="Places to find more of my work"
+          >Github</a
+        >!
+      </p></h8
+    >
+    <h10
+      ><p>
+        Or connect with me on
+        <a
+          href="https://www.linkedin.com/in/brian-hinchey/"
+          title="Places to connect with me"
+          >LinkedIn</a
+        >!
+      </p></h10
+    >
   </div>
 </template>
 
@@ -21,13 +38,13 @@ import ColorPicker from "vue-color-picker-wheel";
 export default {
   name: "App",
   components: {
-    ColorPicker
+    ColorPicker,
   },
   data() {
     return {
-      color: "#ff6600"
+      color: "#ff6600",
     };
-  }
+  },
 };
 </script>
 
@@ -39,14 +56,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   text-align: center;
-  margin-top: 20px;
-  background-color: gray;
+  min-height: 100vh;
+  padding: 20px;
+  background-image: linear-gradient(to right, #c75509, #1668b5);
+  cursor: inherit;
 }
 
 #color-wheel {
   margin-left: auto;
   margin-right: auto;
-  
 }
 
 .selected-color-info {
@@ -54,8 +72,7 @@ export default {
   justify-content: center;
   align-items: center;
   margin-top: 10px;
+  font-size-adjust: inherit;
 }
-
-
 </style>
 
